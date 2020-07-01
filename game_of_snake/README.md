1d2fe28428d88303fbf3268d7379ea54c33aa191
Harris <harrisspahic1190@gmail.com>
2020-07-01 13:01:32 -0400

    Adding snake dir

---
+++ game_of_snake/README.md
@@ -0,0 +1,14 @@
+What is it that we need to do?
+
+The game of snake has a few mechanics,
+
+Snake / Player class:
+- controls
+- VEL
+- length
+==
+- draw
+
+
+
+# Array with elements equal to path 
---
+++ game_of_snake/main.py
@@ -0,0 +1,22 @@
+import pygame
+
+WIN_WIDTH = 1000
+WIN_HEIGHT = 1000
+
+class Head():
+    VEL = 30
+    COLOR = (46, 204, 113)
+
+    def __init():
+        self.x = x
+        self.y = y
+
+
+class Tail():
+    VEL = 30
+    COLOR = (88, 214, 141)
+
+    def __init__():
+        self.x = x
+        self.y = y
+        self.direction = 
